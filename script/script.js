25f56f86e7f70925fb13f9f138b341d27d29d463
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-29 15:00:50 -0500

    Fix clear bug not reseting firstSelected

--- script/script.js
+++ script/script.js
@@ -3,12 +3,9 @@ const preview = document.querySelector("[data-p=preview]");
 const buttons = document.querySelectorAll("button");
 const clearBtn = document.querySelector("[data-btn=clear]");
 
-// Print the number on result text content
 
-clearBtn.addEventListener("click", clear);
 
-let timesFirstNumSelected = 0;
-let bottonPressed = false;
+
 
 const calculator = {
     opperatorSelected: false,
@@ -20,7 +17,7 @@ const calculator = {
     result: "",
 };
 
-console.table(calculator);
+clearBtn.addEventListener("click", clear);
 
 buttons.forEach(btn => btn.addEventListener("click", () => {
 
@@ -31,7 +28,6 @@ buttons.forEach(btn => btn.addEventListener("click", () => {
 
     // Equals button behaviour
     if (btn.getAttribute("data-btn") === "equals"){
-        console.table(`Soy el calculator en el button behaviour ${console.table(calculator)}`);
         makeResult();
         return;
     }
@@ -74,17 +70,8 @@ buttons.forEach(btn => btn.addEventListener("click", () => {
             if(calculator.firstSelected){
                 return;
             }
-            timesFirstNumSelected ++;
-            console.log({timesFirstNumSelected});
-            
-            console.log(`soy el calculator en el momento que están guardando mi primer número}`);
-            console.table(calculator);
 
-// Por aquí está el erroor!
             calculator.firstNum += btn.textContent;
-
-            console.log(`Me acaban de guardar en el calculator y valgo: ${calculator["firstNum"]}`);
-            console.table(calculator);
             calculator.result = String(calculator.firstNum);
             result.textContent = calculator.result;
             calculator.firstSelected = true;
@@ -99,10 +86,9 @@ buttons.forEach(btn => btn.addEventListener("click", () => {
         result.textContent = calculator.result;
         calculator.preview = operate(calculator.firstNum,calculator.secondNum,calculator.opperator);
         preview.textContent = calculator.preview;
-        console.table(`Soy el calculator después de guardar el segundo número ${console.table(calculator)}`);
         return;
     }
-    console.log(btn.textContent);
+
 
 
 }));
@@ -116,19 +102,12 @@ function makeResult(){
     if (!calculator.firstNum && !calculator.opperator && !calculator.secondNum){
         return;
     }
-    console.log("Soy el resultado antes que me den al preview")
-    console.table(calculator);
-    console.log("Soy el resultado después que me den al preview")
     calculator.result = calculator.preview;
-    console.table(calculator);
     result.textContent = calculator.result;
     preview.textContent = "0";
     resetValues();
-    console.table(calculator);
 
     // por aquí esta el bucle, creo
-    console.log("voy a volver a guardar el primer valor");
-    console.log(`Soy el calculator en el make result`);
 
 }
 
@@ -176,7 +155,6 @@ function operate(firstNum, secondNum, opperand){
         return divide(parseInt(firstNum), parseInt(secondNum));
     }
     if (opperand === "xy"){
-        console.log("hola");
         return pow(parseInt(firstNum), parseInt(secondNum));
     }
     
@@ -217,23 +195,9 @@ function removeTransition(e){
     bottonPressed = false;
     const btn = document.querySelector(`[data-key="${e.keyCode}"]`);
     if (!btn) return;
-    console.log(btn);
     btn.classList.remove("active");
 }
 
-
-function clear(){
-    resetValues();
-    calculator.result = "";
-    calculator.firstNum = 0;
-    preview.textContent = "0";
-    result.textContent = "0";
-    firstSelected = false;
-    console.log(`Soy el calculator despues de clear`);
-    console.table(calculator);
-}
-
-// For clearing values on equals and when linking multiply operations
 
 function resetValues(){
     calculator.preview = "";
@@ -241,10 +205,21 @@ function resetValues(){
     calculator.firstSelected = true;
     calculator.firstNum = parseInt(calculator.result);
     calculator.opperator = "";
-    calculator.secondNum = 0;
-    console.log(`Soy el calculator despues de resetValues`);
+    calculator.secondNum = "";
     console.table(calculator);
-
 }
+
+function clear(){
+    resetValues();
+    calculator.result = "";
+    calculator.firstNum = "";
+    preview.textContent = "0";
+    result.textContent = "0";
+    calculator.firstSelected = false;
+    console.table(calculator);
+}
+
+// For clearing values on equals and when linking multiply operations
+
 
 window.addEventListener("keyup", removeTransition);
